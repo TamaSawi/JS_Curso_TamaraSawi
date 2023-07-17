74fe49fd9056419fc6cb801b74f54267ac22c19d
TamaSawi <tami.sawi@gmail.com>
2023-07-17 20:08:51 -0300

    Se agrega funcion

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 //NOTA: El número es aleatorio entre 1 y 10
-const queNumeroEs = Math.floor(Math.random() * 10) + 1;
 
+const queNumeroEs = Math.floor(Math.random() * 10) + 1;
 let intentos = 3;
 
 while (intentos > 0) {
   const adivinar = parseInt(prompt("Adiviná un número entre 1 y 10:"));
+
+  const resultado = verificarAdivinanza(adivinar, queNumeroEs);
+  alert(resultado);
+
   if (adivinar === queNumeroEs) {
-    alert("Muy bien, adivinaste el número correcto.");
     break;
   } else {
     intentos--;
 
     if (intentos > 0) {
-      alert(`El número es incorrecto Te quedan ${intentos} intento(s).`);
+      alert(`Te quedan ${intentos} intento(s).`);
     } else {
       alert(`¡Perdiste! El número correcto era ${queNumeroEs}.`);
     }
+  }
+}
+
+function verificarAdivinanza(adivinar, queNumeroEs) {
+  if (adivinar === queNumeroEs) {
+    return "Muy bien, adivinaste el número correcto.";
+  } else {
+    return "El número es incorrecto.";
   }
 }
